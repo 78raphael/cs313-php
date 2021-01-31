a1d69290f307ac8741290d56cf35a362fbcb6e01
78raphael <raphael78@gmail.com>
2021-01-30 23:31:39 -0700

    Updating PostGreSQL queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,34 +1,37 @@
+CREATE DATABASE life_coach;
+
+CREATE TYPE user_status_type AS ENUM ('admin', 'basic', 'guest');
 CREATE TABLE users  (
-  id int SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   first_name varchar(50) NOT NULL,
   last_name varchar(50) NOT NULL,
   password varchar(255) NOT NULL,
   email varchar(50) NOT NULL,
-  status status_type,
+  status user_status_type,
   active BOOLEAN DEFAULT false,
   created_at timestamp NOT NULL,
-  updated_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL
 );
 
-CREATE TYPE status_type AS ENUM ('active','on hold','deactive','archived');
+CREATE TYPE session_status_type AS ENUM ('active','on hold','deactive','archived');
 CREATE TABLE sessions (
-  id int SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name varchar(75) NOT NULL,
   description varchar DEFAULT NULL,
   image varchar(100) DEFAULT NULL,
   price float DEFAULT NULL,
-  status status_type,
+  status session_status_type,
   created_at timestamp NOT NULL,
-  updated_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL
 );
 
-CREATE TYPE status_type AS ENUM ('archived','pending','requested','in progress');
+CREATE TYPE appointment_status_type AS ENUM ('archived','pending','requested','in progress');
 CREATE TABLE appointments (
-  id int SERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   user_id int NOT NULL,
   session_id int NOT NULL,
   appt_time timestamp NULL DEFAULT NULL,
-  status status_type,
+  status appointment_status_type,
   created_at timestamp NOT NULL,
   updated_at timestamp NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
@@ -36,7 +39,7 @@ CREATE TABLE appointments (
 );
 
 CREATE TABLE reviews (
-  id int SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   session_id int NOT NULL,
   user_id int NOT NULL,
   notes text,
