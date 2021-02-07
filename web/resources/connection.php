1c0e99fce8ce99ed9cf203bbbf3493f8fe81d63b
78raphael <raphael78@gmail.com>
2021-02-06 22:51:57 -0700

    Further testing

--- web/resources/connection.php
+++ web/resources/connection.php
@@ -34,7 +34,7 @@ function connector() {
       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      return 'INSIDE CONNECTOR - TRY<br>'; // <—————————————————————————————————— TESTING
+      return $db;
     }
     catch (PDOException $ex)  {
       echo 'Error!: ' . $ex->getMessage();
