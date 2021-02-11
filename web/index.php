a381a4a28d9ee3ffcc1fa2744af1262fdbed4910
78raphael <raphael78@gmail.com>
2021-02-11 13:28:15 -0700

    Testing team activity week 6

--- web/index.php
+++ web/index.php
@@ -69,14 +69,11 @@ switch($action)
     include 'views/week5/index.php';
     break;
   case 'team6':
-    echo 'before require_once<br>';
+
     require_once 'models/team6.php';
     $toPrint = '';
 
-    echo 'after require_once<br>';
-    echo 'before getTopics()<br>';
     $topics = getTopics();
-    var_dump($topics);
 
     if(isset($_POST['book'])) {
       $book = filter_input(INPUT_POST, 'book', FILTER_SANITIZE_STRING);
